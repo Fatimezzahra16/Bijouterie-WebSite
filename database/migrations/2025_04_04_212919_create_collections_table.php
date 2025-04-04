849c4a1d88c67db4439a03545b8912a5bd9ea3ac
fati <hamdaouifatimaezzahra16@gmail.com>
2025-04-04 21:33:43 +0000

    Ajout des migrations et corrections de la base de données

---
+++ database/migrations/2025_04_04_212919_create_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 100);
+            $table->text('description')->nullable();
+            $table->foreignId('admin_id')->constrained('admins')->onDelete('set null');
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections');
+    }
+};
